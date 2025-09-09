9bd41681f072878a395f4ac3f4e9b7ee53aba2bc
VITOR MIGUEL RODRIGUES CEZARIO <25132565@CFP127ALUNO.LOCAL>
2025-09-09 16:58:13 -0300

    Criado e finalizado Função

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const botaoBuscarDog = document.getElementById('buscar-dog')
+
+
+/* GET do para raças de cachorros */
+async function buscarImagens (raca) {
+    const url = `https://dog.ceo/api/breed/${raca}/images`
+    const response  = await fetch(url)
+    const imagens   =  await response.json()
+    const itens     = imagens.message
+    let i = 0 
+    let container = document.getElementById('receber-dog')
+    
+    while(i < itens.length){
+        let imagem = document.createElement('img')
+        container.appendChild(imagem)
+        let imagemcachorro = itens[i]
+        imagem.src = imagemcachorro
+        i++
+    }
+    return imagens.message
+}
+
+function carregarImagens() {
+    const raca = document.getElementById('cachorros').value.toLowerCase()
+    buscarImagens(raca)
+}
+
+
+
+botaoBuscarDog.addEventListener('click', carregarImagens)
